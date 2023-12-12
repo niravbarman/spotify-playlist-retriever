bd96595cab36ca6bfe8519d0da99876f469fc4f1
niravbarman <niravbarman@gmail.com>
2023-12-12 17:13:49 -0500

    Updated to now get up to 50 of a user's created playlists

--- src/App.js
+++ src/App.js
@@ -62,22 +62,28 @@ function App() {
 
   const getPlaylists = async (e) => {
     //e.preventDefault()
-    const {data} = await axios.get("https://api.spotify.com/v1/me/playlists", {
+    const userId = 'niravbarman';
+    const url = `https://api.spotify.com/v1/users/${userId}/playlists?limit=50`;
+    const {data} = await axios.get(url, {
       headers: {
         Authorization: `Bearer ${token}`
-      },
+      }
     })
 
-    console.log(data)
-    setPlaylists(data.items)
+    const playlists = data.items
+    
+    setPlaylists(playlists.filter((playlist) => {
+      return playlist.owner.id === userId}))
   }
 
   const renderPlaylists = () => {
     return playlists.map(playlist => (
+      <a href={playlist.external_urls["spotify"]}>
         <div key={playlist.id}>
             {playlist.name}
             {playlist.images.length ? <img width={"100%"} src={playlist.images[0].url} alt=""/> : <div>No Image</div>}
         </div>
+      </a>
     ))
   }
 
@@ -85,17 +91,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Playlist Curator: Spotify</h1>
-        {!token ?
-          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES_URL_PARAM}`}>Login to Spotify</a>
-          : <button onClick={logout}>Logout</button>
-        }
+          {!token ?
+            <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES_URL_PARAM}`}>Login to Spotify</a>
+            : <button onClick={logout}>Logout</button>
+          }
 
-        {token ?
-          <button onClick={getPlaylists}>Get Playlists</button>
-          : <h2>Please Login</h2>
-        }
+          {token ?
+            <button onClick={getPlaylists}>Get Playlists</button>
+            : <h2>Please Login</h2>
+          }
 
-        {renderPlaylists()}
+          {renderPlaylists()}
       </header>
     </div>
   );
